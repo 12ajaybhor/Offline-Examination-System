200c6eafaafdce56c090702d9318ad2ab63f2776
Ajay Bhor <12ajaybhor@gmail.com>
2026-02-06 13:40:34 +0530

    Restrict Teacher Access: Removed question management, added student view

--- OfflineExaminationSystem/Forms/TeacherDashboard.Designer.vb
+++ OfflineExaminationSystem/Forms/TeacherDashboard.Designer.vb
@@ -23,24 +23,8 @@ Partial Class TeacherDashboard
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.tabQuestions = New System.Windows.Forms.TabPage()
-        Me.dgvQuestions = New System.Windows.Forms.DataGridView()
-        Me.grpAddQuestion = New System.Windows.Forms.GroupBox()
-        Me.btnAddQuestion = New System.Windows.Forms.Button()
-        Me.cmbCorrectOpt = New System.Windows.Forms.ComboBox()
-        Me.lblCorrect = New System.Windows.Forms.Label()
-        Me.txtOptD = New System.Windows.Forms.TextBox()
-        Me.lblOptD = New System.Windows.Forms.Label()
-        Me.txtOptC = New System.Windows.Forms.TextBox()
-        Me.lblOptC = New System.Windows.Forms.Label()
-        Me.txtOptB = New System.Windows.Forms.TextBox()
-        Me.lblOptB = New System.Windows.Forms.Label()
-        Me.txtOptA = New System.Windows.Forms.TextBox()
-        Me.lblOptA = New System.Windows.Forms.Label()
-        Me.txtQuestion = New System.Windows.Forms.TextBox()
-        Me.lblQuestion = New System.Windows.Forms.Label()
-        Me.cmbExamSelect = New System.Windows.Forms.ComboBox()
-        Me.lblSelectExam = New System.Windows.Forms.Label()
+        Me.tabStudents = New System.Windows.Forms.TabPage()
+        Me.dgvStudents = New System.Windows.Forms.DataGridView()
         Me.tabResults = New System.Windows.Forms.TabPage()
         Me.dgvResults = New System.Windows.Forms.DataGridView()
         Me.tabAnalytics = New System.Windows.Forms.TabPage()
@@ -49,9 +33,8 @@ Partial Class TeacherDashboard
         Me.lblWelcome = New System.Windows.Forms.Label()
         Me.btnLogout = New System.Windows.Forms.Button()
         Me.TabControl1.SuspendLayout()
-        Me.tabQuestions.SuspendLayout()
-        CType(Me.dgvQuestions, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.grpAddQuestion.SuspendLayout()
+        Me.tabStudents.SuspendLayout()
+        CType(Me.dgvStudents, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.tabResults.SuspendLayout()
         CType(Me.dgvResults, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.tabAnalytics.SuspendLayout()
@@ -60,7 +43,7 @@ Partial Class TeacherDashboard
         '
         'TabControl1
         '
-        Me.TabControl1.Controls.Add(Me.tabQuestions)
+        Me.TabControl1.Controls.Add(Me.tabStudents)
         Me.TabControl1.Controls.Add(Me.tabResults)
         Me.TabControl1.Controls.Add(Me.tabAnalytics)
         Me.TabControl1.Location = New System.Drawing.Point(12, 49)
@@ -69,176 +52,25 @@ Partial Class TeacherDashboard
         Me.TabControl1.Size = New System.Drawing.Size(760, 500)
         Me.TabControl1.TabIndex = 2
         '
-        'tabQuestions
+        'tabStudents
         '
-        Me.tabQuestions.Controls.Add(Me.dgvQuestions)
-        Me.tabQuestions.Controls.Add(Me.grpAddQuestion)
-        Me.tabQuestions.Location = New System.Drawing.Point(4, 22)
-        Me.tabQuestions.Name = "tabQuestions"
-        Me.tabQuestions.Padding = New System.Windows.Forms.Padding(3)
-        Me.tabQuestions.Size = New System.Drawing.Size(752, 474)
-        Me.tabQuestions.TabIndex = 1
-        Me.tabQuestions.Text = "Manage Questions"
-        Me.tabQuestions.UseVisualStyleBackColor = True
+        Me.tabStudents.Controls.Add(Me.dgvStudents)
+        Me.tabStudents.Location = New System.Drawing.Point(4, 22)
+        Me.tabStudents.Name = "tabStudents"
+        Me.tabStudents.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabStudents.Size = New System.Drawing.Size(752, 474)
+        Me.tabStudents.TabIndex = 0
+        Me.tabStudents.Text = "View Students"
+        Me.tabStudents.UseVisualStyleBackColor = True
         '
-        'dgvQuestions
+        'dgvStudents
         '
-        Me.dgvQuestions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvQuestions.Location = New System.Drawing.Point(6, 230)
-        Me.dgvQuestions.Name = "dgvQuestions"
-        Me.dgvQuestions.Size = New System.Drawing.Size(740, 238)
-        Me.dgvQuestions.TabIndex = 1
-        '
-        'grpAddQuestion
-        '
-        Me.grpAddQuestion.Controls.Add(Me.btnAddQuestion)
-        Me.grpAddQuestion.Controls.Add(Me.cmbCorrectOpt)
-        Me.grpAddQuestion.Controls.Add(Me.lblCorrect)
-        Me.grpAddQuestion.Controls.Add(Me.txtOptD)
-        Me.grpAddQuestion.Controls.Add(Me.lblOptD)
-        Me.grpAddQuestion.Controls.Add(Me.txtOptC)
-        Me.grpAddQuestion.Controls.Add(Me.lblOptC)
-        Me.grpAddQuestion.Controls.Add(Me.txtOptB)
-        Me.grpAddQuestion.Controls.Add(Me.lblOptB)
-        Me.grpAddQuestion.Controls.Add(Me.txtOptA)
-        Me.grpAddQuestion.Controls.Add(Me.lblOptA)
-        Me.grpAddQuestion.Controls.Add(Me.txtQuestion)
-        Me.grpAddQuestion.Controls.Add(Me.lblQuestion)
-        Me.grpAddQuestion.Controls.Add(Me.cmbExamSelect)
-        Me.grpAddQuestion.Controls.Add(Me.lblSelectExam)
-        Me.grpAddQuestion.Location = New System.Drawing.Point(6, 6)
-        Me.grpAddQuestion.Name = "grpAddQuestion"
-        Me.grpAddQuestion.Size = New System.Drawing.Size(740, 212)
-        Me.grpAddQuestion.TabIndex = 0
-        Me.grpAddQuestion.TabStop = False
-        Me.grpAddQuestion.Text = "Add New Question"
-        '
-        'btnAddQuestion
-        '
-        Me.btnAddQuestion.Location = New System.Drawing.Point(500, 168)
-        Me.btnAddQuestion.Name = "btnAddQuestion"
-        Me.btnAddQuestion.Size = New System.Drawing.Size(120, 30)
-        Me.btnAddQuestion.TabIndex = 14
-        Me.btnAddQuestion.Text = "Add Question"
-        Me.btnAddQuestion.UseVisualStyleBackColor = True
-        '
-        'cmbCorrectOpt
-        '
-        Me.cmbCorrectOpt.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cmbCorrectOpt.FormattingEnabled = True
-        Me.cmbCorrectOpt.Items.AddRange(New Object() {"A", "B", "C", "D"})
-        Me.cmbCorrectOpt.Location = New System.Drawing.Point(100, 172)
-        Me.cmbCorrectOpt.Name = "cmbCorrectOpt"
-        Me.cmbCorrectOpt.Size = New System.Drawing.Size(100, 21)
-        Me.cmbCorrectOpt.TabIndex = 13
-        '
-        'lblCorrect
-        '
-        Me.lblCorrect.AutoSize = True
-        Me.lblCorrect.Location = New System.Drawing.Point(20, 175)
-        Me.lblCorrect.Name = "lblCorrect"
-        Me.lblCorrect.Size = New System.Drawing.Size(79, 13)
-        Me.lblCorrect.TabIndex = 12
-        Me.lblCorrect.Text = "Correct Option:"
-        '
-        'txtOptD
-        '
-        Me.txtOptD.Location = New System.Drawing.Point(400, 137)
-        Me.txtOptD.Name = "txtOptD"
-        Me.txtOptD.Size = New System.Drawing.Size(200, 20)
-        Me.txtOptD.TabIndex = 11
-        '
-        'lblOptD
-        '
-        Me.lblOptD.AutoSize = True
-        Me.lblOptD.Location = New System.Drawing.Point(320, 140)
-        Me.lblOptD.Name = "lblOptD"
-        Me.lblOptD.Size = New System.Drawing.Size(52, 13)
-        Me.lblOptD.TabIndex = 10
-        Me.lblOptD.Text = "Option D:"
-        '
-        'txtOptC
-        '
-        Me.txtOptC.Location = New System.Drawing.Point(100, 137)
-        Me.txtOptC.Name = "txtOptC"
-        Me.txtOptC.Size = New System.Drawing.Size(200, 20)
-        Me.txtOptC.TabIndex = 9
-        '
-        'lblOptC
-        '
-        Me.lblOptC.AutoSize = True
-        Me.lblOptC.Location = New System.Drawing.Point(20, 140)
-        Me.lblOptC.Name = "lblOptC"
-        Me.lblOptC.Size = New System.Drawing.Size(51, 13)
-        Me.lblOptC.TabIndex = 8
-        Me.lblOptC.Text = "Option C:"
-        '
-        'txtOptB
-        '
-        Me.txtOptB.Location = New System.Drawing.Point(400, 107)
-        Me.txtOptB.Name = "txtOptB"
-        Me.txtOptB.Size = New System.Drawing.Size(200, 20)
-        Me.txtOptB.TabIndex = 7
-        '
-        'lblOptB
-        '
-        Me.lblOptB.AutoSize = True
-        Me.lblOptB.Location = New System.Drawing.Point(320, 110)
-        Me.lblOptB.Name = "lblOptB"
-        Me.lblOptB.Size = New System.Drawing.Size(51, 13)
-        Me.lblOptB.TabIndex = 6
-        Me.lblOptB.Text = "Option B:"
-        '
-        'txtOptA
-        '
-        Me.txtOptA.Location = New System.Drawing.Point(100, 107)
-        Me.txtOptA.Name = "txtOptA"
-        Me.txtOptA.Size = New System.Drawing.Size(200, 20)
-        Me.txtOptA.TabIndex = 5
-        '
-        'lblOptA
-        '
-        Me.lblOptA.AutoSize = True
-        Me.lblOptA.Location = New System.Drawing.Point(20, 110)
-        Me.lblOptA.Name = "lblOptA"
-        Me.lblOptA.Size = New System.Drawing.Size(51, 13)
-        Me.lblOptA.TabIndex = 4
-        Me.lblOptA.Text = "Option A:"
-        '
-        'txtQuestion
-        '
-        Me.txtQuestion.Location = New System.Drawing.Point(100, 57)
-        Me.txtQuestion.Multiline = True
-        Me.txtQuestion.Name = "txtQuestion"
-        Me.txtQuestion.Size = New System.Drawing.Size(500, 40)
-        Me.txtQuestion.TabIndex = 3
-        '
-        'lblQuestion
-        '
-        Me.lblQuestion.AutoSize = True
-        Me.lblQuestion.Location = New System.Drawing.Point(20, 60)
-        Me.lblQuestion.Name = "lblQuestion"
-        Me.lblQuestion.Size = New System.Drawing.Size(52, 13)
-        Me.lblQuestion.TabIndex = 2
-        Me.lblQuestion.Text = "Question:"
-        '
-        'cmbExamSelect
-        '
-        Me.cmbExamSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cmbExamSelect.FormattingEnabled = True
-        Me.cmbExamSelect.Location = New System.Drawing.Point(100, 27)
-        Me.cmbExamSelect.Name = "cmbExamSelect"
-        Me.cmbExamSelect.Size = New System.Drawing.Size(250, 21)
-        Me.cmbExamSelect.TabIndex = 1
-        '
-        'lblSelectExam
-        '
-        Me.lblSelectExam.AutoSize = True
-        Me.lblSelectExam.Location = New System.Drawing.Point(20, 30)
-        Me.lblSelectExam.Name = "lblSelectExam"
-        Me.lblSelectExam.Size = New System.Drawing.Size(69, 13)
-        Me.lblSelectExam.TabIndex = 0
-        Me.lblSelectExam.Text = "Select Exam:"
+        Me.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvStudents.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvStudents.Location = New System.Drawing.Point(3, 3)
+        Me.dgvStudents.Name = "dgvStudents"
+        Me.dgvStudents.Size = New System.Drawing.Size(746, 468)
+        Me.dgvStudents.TabIndex = 0
         '
         'tabResults
         '
@@ -247,7 +79,7 @@ Partial Class TeacherDashboard
         Me.tabResults.Name = "tabResults"
         Me.tabResults.Padding = New System.Windows.Forms.Padding(3)
         Me.tabResults.Size = New System.Drawing.Size(752, 474)
-        Me.tabResults.TabIndex = 2
+        Me.tabResults.TabIndex = 1
         Me.tabResults.Text = "View Results"
         Me.tabResults.UseVisualStyleBackColor = True
         '
@@ -268,9 +100,17 @@ Partial Class TeacherDashboard
         Me.tabAnalytics.Name = "tabAnalytics"
         Me.tabAnalytics.Padding = New System.Windows.Forms.Padding(3)
         Me.tabAnalytics.Size = New System.Drawing.Size(752, 474)
-        Me.tabAnalytics.TabIndex = 3
+        Me.tabAnalytics.TabIndex = 2
         Me.tabAnalytics.Text = "Analytics"
         Me.tabAnalytics.UseVisualStyleBackColor = True
+        '
+        'dgvAnalytics
+        '
+        Me.dgvAnalytics.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvAnalytics.Location = New System.Drawing.Point(20, 60)
+        Me.dgvAnalytics.Name = "dgvAnalytics"
+        Me.dgvAnalytics.Size = New System.Drawing.Size(710, 380)
+        Me.dgvAnalytics.TabIndex = 1
         '
         'lblAnalyticsTitle
         '
@@ -280,15 +120,7 @@ Partial Class TeacherDashboard
         Me.lblAnalyticsTitle.Name = "lblAnalyticsTitle"
         Me.lblAnalyticsTitle.Size = New System.Drawing.Size(262, 20)
         Me.lblAnalyticsTitle.TabIndex = 0
-        Me.lblAnalyticsTitle.Text = "Performance Analysis by Exan"
-        '
-        'dgvAnalytics
-        '
-        Me.dgvAnalytics.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvAnalytics.Location = New System.Drawing.Point(20, 60)
-        Me.dgvAnalytics.Name = "dgvAnalytics"
-        Me.dgvAnalytics.Size = New System.Drawing.Size(710, 380)
-        Me.dgvAnalytics.TabIndex = 1
+        Me.lblAnalyticsTitle.Text = "Performance Analysis by Exam"
         '
         'lblWelcome
         '
@@ -322,10 +154,8 @@ Partial Class TeacherDashboard
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Teacher Dashboard"
         Me.TabControl1.ResumeLayout(False)
-        Me.tabQuestions.ResumeLayout(False)
-        CType(Me.dgvQuestions, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.grpAddQuestion.ResumeLayout(False)
-        Me.grpAddQuestion.PerformLayout()
+        Me.tabStudents.ResumeLayout(False)
+        CType(Me.dgvStudents, System.ComponentModel.ISupportInitialize).EndInit()
         Me.tabResults.ResumeLayout(False)
         CType(Me.dgvResults, System.ComponentModel.ISupportInitialize).EndInit()
         Me.tabAnalytics.ResumeLayout(False)
@@ -337,29 +167,13 @@ Partial Class TeacherDashboard
     End Sub
 
     Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents tabQuestions As TabPage
+    Friend WithEvents tabStudents As TabPage
+    Friend WithEvents dgvStudents As DataGridView
     Friend WithEvents tabResults As TabPage
-    Friend WithEvents lblWelcome As Label
-    Friend WithEvents btnLogout As Button
-    Friend WithEvents grpAddQuestion As GroupBox
-    Friend WithEvents dgvQuestions As DataGridView
-    Friend WithEvents lblSelectExam As Label
-    Friend WithEvents cmbExamSelect As ComboBox
-    Friend WithEvents lblQuestion As Label
-    Friend WithEvents txtQuestion As TextBox
-    Friend WithEvents lblOptA As Label
-    Friend WithEvents txtOptA As TextBox
-    Friend WithEvents lblOptB As Label
-    Friend WithEvents txtOptB As TextBox
-    Friend WithEvents lblOptC As Label
-    Friend WithEvents txtOptC As TextBox
-    Friend WithEvents lblOptD As Label
-    Friend WithEvents txtOptD As TextBox
-    Friend WithEvents lblCorrect As Label
-    Friend WithEvents cmbCorrectOpt As ComboBox
-    Friend WithEvents btnAddQuestion As Button
     Friend WithEvents dgvResults As DataGridView
     Friend WithEvents tabAnalytics As TabPage
     Friend WithEvents dgvAnalytics As DataGridView
     Friend WithEvents lblAnalyticsTitle As Label
+    Friend WithEvents lblWelcome As Label
+    Friend WithEvents btnLogout As Button
 End Class
--- OfflineExaminationSystem/Forms/TeacherDashboard.vb
+++ OfflineExaminationSystem/Forms/TeacherDashboard.vb
@@ -4,78 +4,21 @@ Public Class TeacherDashboard
 
     Private Sub TeacherDashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lblWelcome.Text = "Welcome, " & LoginForm.CurrentUserName
-        LoadExams()
-        LoadQuestionsGrid()
+        LoadStudents()
         LoadResults()
         LoadAnalytics()
     End Sub
 
-    ' --- QUESTION MANAGEMENT ---
-    Private Sub LoadExams()
-        Dim dt As DataTable = DatabaseHelper.ExecuteQuery("SELECT * FROM Exams")
-        ' Populate ComboBox for Questions tab
-        cmbExamSelect.DataSource = dt
-        cmbExamSelect.DisplayMember = "Title"
-        cmbExamSelect.ValueMember = "ExamID"
+    ' --- STUDENT VIEW ---
+    Private Sub LoadStudents()
+        Dim query As String = "SELECT FullName, Username FROM Users WHERE Role = 'Student' ORDER BY FullName"
+        Dim dt As DataTable = DatabaseHelper.ExecuteQuery(query)
+        dgvStudents.DataSource = dt
     End Sub
 
-    Private Sub btnAddQuestion_Click(sender As Object, e As EventArgs) Handles btnAddQuestion.Click
-        If cmbExamSelect.SelectedValue Is Nothing Then
-            MessageBox.Show("Please select an Exam first.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            Return
-        End If
-
-        Dim examID As Integer = Convert.ToInt32(cmbExamSelect.SelectedValue)
-        Dim qText As String = txtQuestion.Text.Trim()
-        Dim optA As String = txtOptA.Text.Trim()
-        Dim optB As String = txtOptB.Text.Trim()
-        Dim optC As String = txtOptC.Text.Trim()
-        Dim optD As String = txtOptD.Text.Trim()
-        Dim correct As String = If(cmbCorrectOpt.SelectedItem IsNot Nothing, cmbCorrectOpt.SelectedItem.ToString(), "")
-
-        If String.IsNullOrEmpty(qText) OrElse String.IsNullOrEmpty(optA) OrElse String.IsNullOrEmpty(optB) OrElse String.IsNullOrEmpty(correct) Then
-             MessageBox.Show("Please fill at least Question, Option A, Option B and Correct Option.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-             Return
-        End If
-
-        Dim query As String = "INSERT INTO Questions (ExamID, QuestionText, OptionA, OptionB, OptionC, OptionD, CorrectOption) VALUES (@EID, @QT, @A, @B, @C, @D, @Cor)"
-        Dim params As New Dictionary(Of String, Object)
-        params.Add("@EID", examID)
-        params.Add("@QT", qText)
-        params.Add("@A", optA)
-        params.Add("@B", optB)
-        params.Add("@C", optC)
-        params.Add("@D", optD)
-        params.Add("@Cor", correct)
-
-        Dim rows As Integer = DatabaseHelper.ExecuteNonQueryParams(query, params)
-        If rows > 0 Then
-            MessageBox.Show("Question Added!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
-            txtQuestion.Clear()
-            txtOptA.Clear()
-            txtOptB.Clear()
-            txtOptC.Clear()
-            txtOptD.Clear()
-            cmbCorrectOpt.SelectedIndex = -1
-            LoadQuestionsGrid()
-        End If
-    End Sub
-
-    Private Sub cmbExamSelect_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbExamSelect.SelectedIndexChanged
-        LoadQuestionsGrid()
-    End Sub
-
-    Private Sub LoadQuestionsGrid()
-        If cmbExamSelect.SelectedValue IsNot Nothing AndAlso IsNumeric(cmbExamSelect.SelectedValue) Then
-             Dim dt As DataTable = DatabaseHelper.ExecuteQueryParams("SELECT QuestionID, QuestionText, CorrectOption FROM Questions WHERE ExamID = @EID", 
-                                                                    New Dictionary(Of String, Object) From {{"@EID", cmbExamSelect.SelectedValue}})
-             dgvQuestions.DataSource = dt
-        End If
-    End Sub
-
-     ' --- RESULTS VIEW ---
+    ' --- RESULTS VIEW ---
     Private Sub LoadResults()
-        Dim query As String = "SELECT r.ResultID, u.FullName, ex.Title, r.Score, r.TotalMarks, r.TakenAt FROM Results r JOIN Users u ON r.UserID = u.UserID JOIN Exams ex ON r.ExamID = ex.ExamID ORDER BY r.TakenAt DESC"
+        Dim query As String = "SELECT r.ResultID, u.FullName, ex.Title, r.Score, r.TotalMarks, r.TakenAt FROM Results r JOIN Users u ON r.UserID = u.UserID JOIN Exams ex ON r.ExamID = ex.ExamID WHERE u.Role = 'Student' ORDER BY r.TakenAt DESC"
         Dim dt As DataTable = DatabaseHelper.ExecuteQuery(query)
         dgvResults.DataSource = dt
     End Sub
